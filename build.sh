c0bfa55ff24426c53f9e4f71c07b6d9f0d82b9a5
mox692 <55653825+mox692@users.noreply.github.com>
2021-08-30 03:14:02 +0000

    kernel雛形,makefile作成

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+
 rm -rf ./build/**
 cd ./edk2/ && source ./edksetup.sh && build
-cp /home/vscode/edk2/Build/MosLoaderPkgX64/DEBUG_CLANG38/X64/Loader.efi /home/vscode/mos/build/Loader.efi
-cd /home/vscode/mos/build/ && run_qemu.sh Loader.efi
+cp $HOME/edk2/Build/MosLoaderPkgX64/DEBUG_CLANG38/X64/Loader.efi $HOME/mos/build/Loader.efi
+run_qemu.sh $HOME/mos/build/Loader.efi $HOME/mos/kernel/kernel.elf
