60c80222a37f58ece996dfdb715cae2b26da9d39
mox692 <55653825+mox692@users.noreply.github.com>
2021-09-01 05:21:01 +0000

    printk

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -17,6 +17,22 @@ void operator delete(void* obj) noexcept {
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
 
+char console_buf[sizeof(Console)];
+Console* console;
+
+int printk(const char* format, ...) {
+  va_list ap;
+  int result;
+  char s[1024];
+
+  va_start(ap, format);
+  result = vsprintf(s, format, ap);
+  va_end(ap);
+
+  console->PutString(s);
+  return result;
+}
+
 // const参照型
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
   switch (frame_buffer_config.pixel_format) {
@@ -37,12 +53,10 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
     }
   }
 
-  Console console{*pixel_writer, {0, 0, 0}, {255, 255, 255}}; 
+  console = new(console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
 
-  char buf[128];
-  for (int i = 0; i < 227; ++i) {
-    sprintf(buf, "line %d\n", i);
-    console.PutString(buf);
+  for (int i = 0; i < 27; ++i) {
+    printk("printk: %d\n", i);
   }
 
   while (1) __asm__("hlt");
